6d64cd1d7c01643e74a1a97853c97ef3158ccd5e
lime7 <senseless.v.7@gmail.com>
2022-10-19 17:15:55 +0300

    Test

--- src/utils/makeUrl.js
+++ src/utils/makeUrl.js
@@ -1,6 +1,6 @@
 export const apiBaseUrl =
   process.env.NODE_ENV === "development"
-    ? "https://sv-e.herokuapp.com"
+    ? "http://localhost:3001"
     : `${window.location.origin}/api`;
 
 export const makeUrl = (urlSuff) => `${apiBaseUrl}/${urlSuff}`;
