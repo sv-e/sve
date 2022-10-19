2922864fd93723b2da234773cc5a6a964dd03524
lime7 <senseless.v.7@gmail.com>
2022-10-19 15:37:34 +0300

    Test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("./api/db.json");
+// const router = jsonServer.router("./api/db.json");
 const middlewares = jsonServer.defaults({
   static: "./build"
 });
@@ -8,7 +8,7 @@ const middlewares = jsonServer.defaults({
 const PORT = process.env.PORT || 3001;
 
 server.use(middlewares);
-server.use(router);
+// server.use(router);
 
 server.listen(PORT, () => {
   console.log(">>>>> Server is running");
