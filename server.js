1715757e4833959cc7900d32a0dd42bc4671e4f8
lime7 <senseless.v.7@gmail.com>
2022-11-19 15:23:58 +0200

    update

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("./api/db.json");
@@ -9,6 +14,13 @@ const PORT = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(router);
+
+app.get("*", (req, res) => {
+  let url = path.join(__dirname, "../client/build", "index.html");
+  if (!url.startsWith("/app/")) // since we're on local windows
+    url = url.substring(1);
+  res.sendFile(url);
+});
 
 server.listen(PORT, () => {
   console.log(">>>>> Server is running");
